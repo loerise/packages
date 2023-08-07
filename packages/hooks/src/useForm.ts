c852ff4f82f570cb42590875d1892ece3e67e5e3
Lester Lau <loerise@gmail.com>
2023-08-07 17:35:44 +0800

    feat: 更新车牌正则

--- packages/hooks/src/useForm.ts
+++ packages/hooks/src/useForm.ts
@@ -287,7 +287,7 @@ export const useForm: UseFrom = (options = {}) => {
           case "licensePlate":
             if (
               !isEmpty(value) &&
-              !/^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-HJ-NP-Z][A-HJ-NP-Z0-9]{4,5}[A-HJ-NP-Z0-9挂学警港澳]$/.test(
+              !/^[京津沪渝冀晋辽吉黑苏浙皖川闽赣鲁豫鄂湘粤琼黔滇陕甘青台蒙桂藏宁新领使临][A-HJ-NP-Z][A-HJ-NP-Z0-9]{4,5}[A-HJ-NP-Z0-9挂学警港澳]$/.test(
                 String(value)
               )
             ) {
